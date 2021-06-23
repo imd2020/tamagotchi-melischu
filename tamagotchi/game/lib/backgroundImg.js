1af18ecacaca227493fb8eff195fbfb8235fa258
melischu <73801670+melischu@users.noreply.github.com>
2021-06-23 23:09:34 +0200

    Create backgroundImg.js

---
+++ tamagotchi/game/lib/backgroundImg.js
@@ -0,0 +1,52 @@
+export default class BackgroundImg {
+  constructor(x, y, s) {
+    this.xPos = x;
+    this.yPos = y;
+    this.scale = s;
+    this.startscreen_Img = loadImage("lib\\screens\\startscreen.png");
+    this.seed_Img = loadImage("lib\\screens\\seed.png");
+    this.germ_Img = loadImage("lib\\screens\\germ.png");
+    this.buds_Img = loadImage("lib\\screens\\buds.png");
+    this.plant_Img = loadImage("lib\\screens\\plant.png");
+    this.soil_Img = loadImage("lib\\screens\\soil.png");
+    this.withered_germ_Img = loadImage("lib\\screens\\withered_germ.png");
+    this.withered_buds_Img = loadImage("lib\\screens\\withered_buds.png");
+    this.withered_plant_Img = loadImage("lib\\screens\\withered_plant.png");
+    this.positiv_endscreen_Img = loadImage(
+      "lib\\screens\\negativ_endscreen.png"
+    );
+    this.negativ_endscreen_Img = loadImage(
+      "lib\\screens\\positiv_endscreen.png"
+    );
+  }
+
+  displayScreen(state) {
+    let pictureSource;
+    if (state === "start") {
+      pictureSource = this.startscreen_Img;
+    } else if (state === "seed") {
+      pictureSource = this.seed_Img;
+    } else if (state === "germ") {
+      pictureSource = this.germ_Img;
+    } else if (state === "buds") {
+      pictureSource = this.buds_Img;
+    } else if (state === "plant") {
+      pictureSource = this.plant_Img;
+    } else if (state === "soil") {
+      pictureSource = this.soil_Img;
+    } else if (state === "withered_germ") {
+      pictureSource = this.withered_germ_Img;
+    } else if (state === "withered_buds") {
+      pictureSource = this.withered_buds_Img;
+    } else if (state === "withered_plant") {
+      pictureSource = this.withered_plant_Img;
+    } else if (state === "positiv_endscreen") {
+      pictureSource = this.positiv_endscreen_Img;
+    } else if (state === "negativ_endscreen") {
+      pictureSource = this.negativ_endscreen_Img;
+    } else {
+      console.log("No state given!");
+    }
+    image(pictureSource, 0, 0);
+  }
+}
