654650b7ef5710470c24d5da518add1f74e1a54a
melischu <73801670+melischu@users.noreply.github.com>
2021-06-23 23:10:39 +0200

    Create states.js

---
+++ tamagotchi/game/lib/states.js
@@ -0,0 +1,14 @@
+export default class plantStates {
+  constructor(nextPositivState = "false", nextNegativState = "false") {
+    this.nextPositivState = nextPositivState;
+    this.nextNegativState = nextNegativState;
+  }
+
+  nextState(stateChange) {
+    if (stateChange === "positiv") {
+      return this.nextPositivState;
+    } else if (stateChange === "negativ") {
+      return this.nextNegativState;
+    }
+  }
+}
