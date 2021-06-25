266efd58095d585982671a070827d37aa3318bfc
melischu <73801670+melischu@users.noreply.github.com>
2021-06-25 17:22:33 +0200

    Update states.js

--- tamagotchi/game/lib/states.js
+++ tamagotchi/game/lib/states.js
@@ -1,9 +1,10 @@
 export default class PlantStates {
+  //initialising constructor variables
   constructor(nextPositivState = "false", nextNegativState = "false") {
     this.nextPositivState = nextPositivState;
     this.nextNegativState = nextNegativState;
   }
-
+  //initialising positiv an negativ states for state change
   nextState(stateChange) {
     if (stateChange === "positiv") {
       return this.nextPositivState;
