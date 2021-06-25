853d91505e42c739ca59c8589ce8c737669a122b
melischu <73801670+melischu@users.noreply.github.com>
2021-06-25 17:22:37 +0200

    Update textOnScreen.js

--- tamagotchi/game/lib/textOnScreen.js
+++ tamagotchi/game/lib/textOnScreen.js
@@ -1,11 +1,12 @@
 export default class TextOnScreen {
+  //initialising constructor variables
   constructor(x, y, s, text) {
     this.xPos = x;
     this.yPos = y;
     this.scale = s;
     this.text = text;
   }
-
+  //text on screen information
   display() {
     fill(100, 20, 54);
     textAlign(CENTER);
